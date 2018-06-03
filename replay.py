aca01a03181fc960d684bb5cc7e542972bf86076
Praveen Palanisamy <praveen.palanisamy@outlook.com>
2018-06-03 19:28:30 -0400

    W.I.P Replay memory: Updated the set priorities method. Added descriptive comments. Updates sampling probability when the set priorities method is called

--- replay.py
+++ replay.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 N_Step_Transition = namedtuple('N_Step_Transition', ['S_t', 'A_t', 'R_ttpB', 'Gamma_ttpB', 'qS_t', 'S_tpn', 'qS_tpn', 'key'])
-Prioritized_N_Step_Transition = namedtuple('Prioritized_N_Step_Transition', ['St', 'At', 'R_ttpB', 'Gamma_ttpB',
-                                                                             'S_tpn', 'priority', 'key'])
+
+
 class ReplayMemory(object):
     def __init__(self, soft_capacity, params):
         self.soft_capacity = soft_capacity
@@ -13,11 +13,29 @@ class ReplayMemory(object):
         self.counter = 0
         self.alpha = params['priority_exponent']
         self.priorities = dict()
+        self.sample_probabilities = dict()
 
-    def update_sample_probability(self):
+    def update_sample_probabilities(self):
+        """
+        Updates the probability of sampling an experience from the replay memory
+        :return:
+        """
         priorities = self.priorities
         prob = [p**self.alpha/ sum(priorities.values())  for p in priorities.values()]
-        self.prob = {k:v for k in priorities.keys() for v in prob}
+        self.sample_probabilities.update({k:v for k in priorities.keys() for v in prob})
+
+    def set_priorities(self, new_priorities):
+        """
+        Updates the priorities of experience using the key that uniquely identifies an experience.
+        If a key does not exist, it is added. If a key already exists, the priority value is updated/overwritten.
+        Whenever the priorities are altered/added, the sample probabilities are also updated.
+        :param priorities: A dictionary with experience_key: priority_value key-value pairs
+        :return: None
+        """
+        self.priorities.update(new_priorities)
+        # Update the sample probabilities as well
+        self.update_sample_probabilities()
+
 
     def sample(self, sample_size):
         """
